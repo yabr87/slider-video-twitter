56d9ce43d022eb1c070a2332871d48475651de4f
Yaroslav Bernatovych <108272773+yabr87@users.noreply.github.com>
2024-05-22 15:56:55 +0300

    add/ styles

--- twttrVideo.js
+++ twttrVideo.js
@@ -1,3 +1,65 @@
+var mainSwiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  initialSlide: 0,
+  spaceBetween: 10,
+  updateOnWindowResize: true,
+  watchOverflow: true,
+  centeredSlides: true,
+  // loop: true,
+
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: -1,
+    scale: 0.8,
+    slideShadows: false,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 1,
+    // },
+    // // when window width is >= 480px
+    // 481: {
+    //   slidesPerView: 1.5,
+    // },
+    // when window width is >= 640px
+    // 820: {
+    //   slidesPerView: 1.5,
+    // },
+    // 1280: {
+    //   slidesPerView: 1.9,
+    // },
+    // 1600: {
+    //   slidesPerView: 2.5,
+    // },
+  },
+
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+
+  thumbs: {
+    swiper: {
+      el: '.swiper-preview',
+      slidesPerView: 5,
+      // loop: true,
+    },
+  },
+});
+
+// var thumbsSwiper = new Swiper('.swiper-preview', {
+//   // Настройки миниатюрного Swiper
+//   slidesPerView: 3,
+//   // loop: true,
+//   // centeredSlides: true,
+// });
+
+// // Синхронизация основного и миниатюрного Swiper
+// mainSwiper.controller.control = thumbsSwiper;
+// thumbsSwiper.controller.control = mainSwiper;
+
 document.addEventListener('DOMContentLoaded', function () {
   MicroModal.init({
     onShow: (modal) => {
